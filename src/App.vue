2931eb36def9d1baf8fa2b6d89e5bba87dba4097
Przemyslaw Gawlowski <gawlowski.przemyslaw@gmail.com>
2019-10-08 11:14:44 +0200

    Add slide-fade transition

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="app">
         <app-header></app-header>
-        <router-view></router-view>
+        <transition name="slide-fade" mode="out-in">
+            <router-view></router-view>
+        </transition>
         <img alt="Vue logo" src="./assets/logo.png">
         <HelloWorld msg="Welcome to Your Vue.js App"/>
     </div>
@@ -28,5 +30,15 @@
         text-align: center;
         color: #2c3e50;
         margin-top: 60px;
+    },
+    .slide-fade-enter-active {
+        transition: all .3s ease;
+    }
+    .slide-fade-leave-active {
+        transition: all .3s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+    }
+    .slide-fade-enter, .slide-fade-leave-to {
+        transform: translateX(10px);
+        opacity: 0;
     }
 </style>
