082dd7f3653bf61b4cc24b86823dba3e65d7ce66
Przemyslaw Gawlowski <gawlowski.przemyslaw@gmail.com>
2019-10-08 10:25:39 +0200

    Create component for Movie Details

---
+++ src/components/MovieDetails.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Movie details {{ movie_id }}</h1>
+        <button @click="navigateToHome()">Back to Home</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MovieDetails",
+        data() {
+            return {
+                movie_id: this.$route.params.movie_id
+            }
+        },
+        watch: {
+            '$route'(to) {
+                this.movie_id = to.params.id;
+            }
+        },
+        methods: {
+            navigateToHome() {
+                this.$router.push('/')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import Home from './components/Home'
 import Stats from "./components/Stats";
+import MovieDetails from "./components/MovieDetails";
 
 export const routes = [
     {
         path: '',
         component: Home
+    },
+    {
+        path: '/movie/:movie_id/details',
+        component: MovieDetails
     },
     {
         path: '/stats',
