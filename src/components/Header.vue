b31bd6770a4bfc8d333fa73e62eb9d4ff060fb0e
Przemyslaw Gawlowski <gawlowski.przemyslaw@gmail.com>
2019-10-08 09:53:29 +0200

    Create Header component for navigation

---
+++ src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <ul>
+            <li>
+                <router-link to="/">Home</router-link>
+            </li>
+            <li>
+                <router-link to="/stats">Stats</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Header"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,13 @@
 import Home from './components/Home'
+import Stats from "./components/Stats";
 
 export const routes = [
     {
         path: '',
         component: Home
+    },
+    {
+        path: '/stats',
+        component: Stats
     }
 ];
