9c5476fa24583db2af31b9f65f79fbafc3779e84
Przemyslaw Gawlowski <gawlowski.przemyslaw@gmail.com>
2019-10-09 08:54:23 +0200

    Add the ability to search movies

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 const state = {
     movies: [],
     currentMovie: [],
-    apiKey: 'f5113d82bf25e774efd85825b36c9f09'
+    apiKey: 'f5113d82bf25e774efd85825b36c9f09',
+    foundMovies: []
 };
 
 const getters = {
@@ -12,6 +13,9 @@ const getters = {
     },
     currentMovie: state => {
         return state.currentMovie;
+    },
+    foundMovies: state => {
+        return state.foundMovies;
     }
 };
 
@@ -21,25 +25,35 @@ const mutations = {
     },
     storeCurrentMovie: (state, currentMovie) => {
         state.currentMovie = currentMovie;
+    },
+    storeFoundMovies: (state, foundMovies) => {
+        state.foundMovies = foundMovies;
     }
 };
 
 const actions = {
-    getMovies: ({ state, commit }) => {
+    getMovies: ({state, commit}) => {
         axios.get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${state.apiKey}`)
             .then(response => {
                 commit('storeMovies', response.data.results);
             });
     },
-    getMovieDetails: ({ state, commit }, movieId) => {
+    getMovieDetails: ({state, commit}, movieId) => {
         state.currentMovie = [];
         axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${state.apiKey}`)
             .then(response => {
                 commit('storeCurrentMovie', response.data);
             });
     },
-    storeMovies: ({ commit }, movies) => {
+    storeMovies: ({commit}, movies) => {
         commit('storeMovies', movies);
+    },
+    searchMovies: ({state, commit}, query) => {
+        state.foundMovies = [];
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${state.apiKey}&query=${query}`)
+            .then(response => {
+                commit('storeFoundMovies', response.data.results);
+            });
     }
 };
 
