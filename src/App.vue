c94441af1766da055a5b4fb5116139a7aa5dd9b1
Przemyslaw Gawlowski <gawlowski.przemyslaw@gmail.com>
2019-10-10 12:05:31 +0200

    Reformat

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
     export default {
         name: 'app',
         components: {
-            appHeader : AppHeader,
+            appHeader: AppHeader,
         }
     }
 </script>
@@ -28,9 +28,11 @@
     .slide-fade-enter-active {
         transition: all .2s ease;
     }
+
     .slide-fade-leave-active {
         transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
     }
+
     .slide-fade-enter, .slide-fade-leave-to {
         transform: translateX(10px);
         opacity: 0;
