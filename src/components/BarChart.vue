59cf0e649a30f7aeae2349a8f2b78fb4fb324151
Przemyslaw Gawlowski <gawlowski.przemyslaw@gmail.com>
2019-10-10 08:54:43 +0200

    Add Bar Chart component

---
+++ src/components/BarChart.vue
@@ -0,0 +1,123 @@
+<template>
+    <div id="container" class="svg-container" align="center">
+        <h1>{{ title }}</h1>
+        <svg :width="svgWidth" :height="svgHeight">
+            <g>
+                <rect
+                        v-for="item in data"
+                        class="bar-positive"
+                        :key="item[xKey]"
+                        :x="xScale(item[xKey])"
+                        :y="yScale(0)"
+                        :width="xScale.bandwidth()"
+                        :height="0"
+                >
+                </rect>
+            </g>
+        </svg>
+    </div>
+</template>
+
+<script>
+    import { scaleLinear, scaleBand } from 'd3-scale';
+    import { max, min } from 'd3-array';
+    import { selectAll } from 'd3-selection';
+    import { transition } from 'd3-transition'
+    export default {
+        name: "BarChart",
+        props: {
+            title: String,
+            xKey: String,
+            yKey: String,
+            data: Array
+        },
+        watch: {
+            svgWidth() {
+                this.svgWidth = document.getElementById("container").offsetWidth * 0.75;
+            }
+        },
+        computed: {
+            dataMax() {
+                return max(this.data, d => {
+                    return d[this.yKey];
+                });
+            },
+            dataMin() {
+                return min(this.data, d => {
+                    return d[this.yKey];
+                });
+            },
+            xScale() {
+                return scaleBand()
+                    .rangeRound([0, this.svgWidth])
+                    .padding(0.1)
+                    .domain(this.data.map(d => {
+                        return d[this.xKey];
+                    }));
+            },
+            yScale() {
+                return scaleLinear()
+                    .rangeRound([this.svgHeight, 0])
+                    .domain([this.dataMin > 0 ? 0 : this.dataMin, this.dataMax])
+            },
+            svgHeight() {
+                return this.svgWidth / 1.61803398875;
+            }
+        },
+        mounted() {
+            this.svgWidth = document.getElementById("container").offsetWidth * 0.95;
+            this.addResizeListener();
+            this.animateLoad();
+        },
+        data() {
+            return {
+                svgWidth: 0,
+                redrawToggle: true
+            }
+        },
+        methods: {
+            animateLoad() {
+                transition();
+                selectAll("rect")
+                    .data(this.data)
+                    .transition()
+                    .delay((d, i) => {
+                        return i * 150;
+                    })
+                    .duration(1000)
+                    .attr("y", d => {
+                        return this.yScale(d[this.yKey]);
+                    })
+                    .attr("height", d => {
+                        return this.svgHeight - this.yScale(d[this.yKey]);
+                    });
+            },
+            addResizeListener() {
+                window.addEventListener("resize", () => {
+                    this.$data.redrawToggle = false;
+                    setTimeout(() => {
+                        this.$data.redrawToggle = true;
+                        this.$data.svgWidth = document.getElementById("container").offsetWidth * 0.75;
+                        this.animateLoad();
+                    }, 300);
+                });
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .bar-positive {
+        fill: steelblue;
+        transition: r 0.2s ease-in-out;
+    }
+
+    .svg-container {
+        display: inline-block;
+        position: relative;
+        width: 100%;
+        padding-bottom: 1%;
+        vertical-align: top;
+        overflow: hidden;
+    }
+</style>
--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -3,12 +3,39 @@
         <div class="title">
             <h2>Stats</h2>
         </div>
+        <BarChart title="Bar Chart" xKey="name" yKey="amount" :data="barChartData"></BarChart>
     </section>
 </template>
 
 <script>
+    import BarChart from "./BarChart";
     export default {
-        name: "Stats"
+        name: "Stats",
+        components: {
+            BarChart
+        },
+        data() {
+            return {
+                barChartData: [
+                    {
+                        name: 'Matrix',
+                        amount: 25
+                    },
+                    {
+                        name: 'Lion King',
+                        amount: 40
+                    },
+                    {
+                        name: 'Terminator',
+                        amount: 15
+                    },
+                    {
+                        name: 'Gran Torino',
+                        amount: 56
+                    },
+                ]
+            }
+        }
     }
 </script>
 
